fdd588e15833440f1ae09cba20dbcc9671155c0c
arunabhnewar <newardevil@gmail.com>
2021-08-29 17:46:51 +0600

    inner text changed

--- main.js
+++ main.js
@@ -29,31 +29,31 @@ function displayUser(user) {
 
     // change user email
     document.getElementById('email').addEventListener('mouseover', () => {
-        intro.innerText = `Hi, My name is`
+        intro.innerText = `Hi, My email is`
         display.innerText = `${user.email}`
     })
 
     // change user email
     document.getElementById('dob').addEventListener('mouseover', () => {
-        intro.innerText = `Hi, My name is`
+        intro.innerText = `Hi, My date of birth is`
         display.innerText = `${user.dob.date.slice(0, 10)}`
     })
 
     // change user phone number
     document.getElementById('phone').addEventListener('mouseover', () => {
-        intro.innerText = `Hi, My name is`
+        intro.innerText = `Hi, My phone number is`
         display.innerText = `${user.phone}`
     })
 
     // change user address
     document.getElementById('address').addEventListener('mouseover', () => {
-        intro.innerText = `Hi, My name is`
+        intro.innerText = `Hi, My address is`
         display.innerText = `${user.location.street.number}, ${user.location.city}, ${user.location.state}, ${user.location.country}`
     })
 
     // change user password
     document.getElementById('password').addEventListener('mouseover', () => {
-        intro.innerText = `Hi, My name is`
+        intro.innerText = `Hi, My password is`
         display.innerText = `${user.login.password}`
     })
     console.log(user);
